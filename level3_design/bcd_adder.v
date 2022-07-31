39967a4cc2bf713f88db334afcef2eeeb408b0bb
NaRaK-AK47 <karansanghvi98@gmail.com>
2022-07-31 13:27:12 +0000

    updates of file

--- level3_design/bcd_adder.v
+++ level3_design/bcd_adder.v
@@ -25,31 +25,3 @@ module bcd_adder(a,b,cin,sum,cout);
 
 endmodule
 
-
-module tb_bcdadder;
-
-    reg [3:0] a;
-    reg [3:0] b;
-    reg cin;
-
-    wire [3:0] sum;
-    wire cout;
-
-    bcd_adder uut (
-        .a(a), 
-        .b(b), 
-        .cin(cin), 
-        .sum(sum), 
-        .cout(cout)
-    );
-
-    initial begin
-        a = 0;  b = 0;  cin = 0;   #100;
-        a = 6;  b = 9;  cin = 0;   #100;
-        a = 3;  b = 3;  cin = 1;   #100;
-        a = 4;  b = 5;  cin = 0;   #100;
-        a = 8;  b = 2;  cin = 0;   #100;
-        a = 9;  b = 9;  cin = 1;   #100;
-    end
-      
-endmodule
